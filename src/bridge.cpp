8d02caba065bc0e2492747e81777d3fbd7c4c11a
mayveiscous <wlevi882@gmail.com>
2026-07-23 08:17:15 -0400

    commenting files

--- src/bridge.cpp
+++ src/bridge.cpp
@@ -12,16 +12,20 @@ extern "C" {
 namespace luna {
 
 int bridge_import(lua_State* L) {
+    // check the module name
     const char* name = luaL_checkstring(L, 1);
-
+    
+    // initalize the runtime
     runtime_init();
-
+    
+    // create the PyObject
     PyObject* module = PyImport_ImportModule(name);
     if (!module) {
         errors_raise_from_python(L);
         return 0;
     }
-
+    
+    // push the object and decref
     pyobject_push(L, module, nullptr);
     Py_DECREF(module);
     return 1;
